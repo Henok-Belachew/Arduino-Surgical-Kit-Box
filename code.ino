48cabdf38d6c76a73cf4c28b3e7b72d142ed06da
Henok Belachew <henokbelachewstu@gmail.com>
2023-02-21 00:30:38 +0300

    buzzer added

--- code.ino
+++ code.ino
@@ -6,7 +6,7 @@
 LiquidCrystal_I2C lcd(0x27,16,2);  //0x20 for simulation and 0x27 for prototype
 
 int totalItems = 0;
-int initialDistance = 33;
+int initialDistance = 37;
 // int initialDistance_2 = 33;
 
 
@@ -17,6 +17,7 @@ int initialDistance = 33;
       int SWH2 = 8;
       int switchState1 = 0;
       int switchState2 = LOW;
+      int buzPin = 7;
       //---------------//
       int lastButtonState;    // the previous state of button
       int currentButtonState; // the current state of button
@@ -24,7 +25,7 @@ int initialDistance = 33;
 
 // GLOBAL VARIABLES FOR ULTRASONIC SENSOR
 
-      int distanceInitial = 33;  //Box width
+      int distanceInitial = 37;  //Box width
 
       // 1st ULTRASONIC SENSOR
       int trigPin1 = 5;
@@ -79,8 +80,12 @@ void setup() {
   // Setting Pins for ULTRASONIC SENSORS
           pinMode(trigPin1, OUTPUT);
           pinMode(echoPin1, INPUT);
+          pinMode(buzPin, OUTPUT);
           pinMode(trigPin2, OUTPUT);
           pinMode(echoPin2, INPUT);
+
+  // Starting the SERIAL MONITOR
+  Serial.begin(9600);
 }
 
   
@@ -130,6 +135,8 @@ void loop() {
             duration1 = pulseIn(echoPin1, HIGH);
             distanceCm1= duration1*0.034/2; 
 
+
+            Serial.println(distanceCm1);
             // DISPLAYING MEASURED DISTANCE-1
             // lcd.setCursor(0,0);
             // lcd.print("Distance1:");
@@ -145,43 +152,127 @@ void loop() {
             duration2 = pulseIn(echoPin2, HIGH);
             distanceCm2= duration2*0.034/2; 
 
+            
+            Serial.println(distanceCm2);
+
+
+            // sensor copy
+
+            
+
             // DISPLAYING MEASURED DISTANCE-2
-           // lcd.setCursor(0,1);
-            // lcd.print("Distance2:");
-            // lcd.print(distanceCm1);
-            // lcd.print(" - ");
-            // lcd.print(distanceCm2);
+          //  lcd.setCursor(0,1);
+          //   lcd.print("Dis:");
+          //   lcd.print(distanceCm1);
+          //   lcd.print(" - ");
+          //   lcd.print(distanceCm2);
             // lcd.print(" Cm ");  
 
       // Getting the mode of the machine
       if (digitalRead(ledGreen) == HIGH && switchState2 == LOW) {  //Coutnting Mode
 
-        if(distanceCm1 != initialDistance) {
-          totalItems += 1;
-          delay(2000);  //time required for the users to handl their out from the box
+        if(distanceCm1 < initialDistance - 5 || distanceCm1 > initialDistance + 5) {
+          
+              totalItems += 1;
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(buzPin, LOW);
+              delay(2500);
+          
+         
+            //time required for the users to handl their out from the box
         }
 
-        if(distanceCm2 != initialDistance) {
-          totalItems += 1;
-          delay(2000);  //time required for the users to handl their out from the box
+        if(distanceCm2 < initialDistance - 25  || distanceCm2 > initialDistance + 25) {
+              totalItems += 1;
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(buzPin, LOW);
+              delay(2500);
         }
 
       }
 
       if (digitalRead(ledRed) == HIGH && switchState2 == LOW) {  //Checking Mode
 
-        if(distanceCm1 != initialDistance) {
-          totalItems -= 1;
-          delay(2000);  //time required for the users to handl their out from the box
+        if(distanceCm1 < initialDistance - 5 || distanceCm1 > initialDistance + 5) {
+
+          if(totalItems != 0){
+              totalItems -= 1;
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(buzPin, LOW);
+          }
+          
+          if (totalItems == 0) {
+            for (int i = 0; i < 3; i++) {
+              digitalWrite(ledGreen, HIGH);
+              digitalWrite(ledRed, HIGH);
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(ledGreen, LOW);
+              digitalWrite(ledRed, LOW);
+              digitalWrite(buzPin, LOW);
+              Serial.println("Buzzered!!");
+              delay(500);
+            }
+            
+          }
+
+          if (totalItems == 0) {
+            delay(1000);
+           
+          }
+          else {
+            delay(3000); //Time required for the users to handl their out from the box
+            
+          }
+            
+          
         }
 
-        if(distanceCm2 != initialDistance) {
-          totalItems -= 1;
-          delay(2000);  //time required for the users to handl their out from the box
+
+        // Deacrementing using the 2nd sensor
+        if(distanceCm2 < initialDistance - 25 || distanceCm2 > initialDistance + 25) {
+          if(totalItems != 0){
+              totalItems -= 1;
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(buzPin, LOW);
+          }
+          
+          if (totalItems == 0) {
+            for (int i = 0; i < 3; i++) {
+              digitalWrite(ledGreen, HIGH);
+              digitalWrite(ledRed, HIGH);
+              digitalWrite(buzPin, HIGH);
+              delay(500);
+              digitalWrite(ledGreen, LOW);
+              digitalWrite(ledRed, LOW);
+              digitalWrite(buzPin, LOW);
+              Serial.println("Buzzered!!");
+              delay(500);
+            }
+            
+          }
+
+          if (totalItems == 0) {
+            delay(1000);
+           
+          }
+          else {
+            delay(3000); //Time required for the users to handl their out from the box
+            
+          }
+            
+          
         }
 
       }
 
+      // Displaying the result on Serial Monitor
+      Serial.println(totalItems);
+      
 
       
 
